e02107f1e8f9bf4329c8c80850930ce1bebbb7e5
Anoosha Batool <anooshabatool2001@gmail.com>
2024-06-23 22:25:30 +0500

    Anoosha's Commit

---
+++ lib/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=surface_pressure";
+
+  Future<List<double>> fetchSurfacePressure() async {
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<double> surfacePressure = [];
+      for (var pressure in data['hourly']['surface_pressure']) {
+        surfacePressure.add(pressure.toDouble());
+      }
+      return surfacePressure;
+    } else {
+      throw Exception('Failed to load surface pressure data');
+    }
+  }
+}
