441cb88ec9a8550ce5928174b0ae9de3c28f682f
Yumna Afsar <83974096+yumnaafsar@users.noreply.github.com>
2024-06-24 11:06:39 -0700

    Update simulation.dart

--- lib/dewpoint/simulation.dart
+++ lib/dewpoint/simulation.dart
@@ -1,19 +1,1 @@
-// monte_carlo.dart
 
-import 'dart:math';
-
-List<double> monteCarloSimulation(List<double> historicalData, int simulations) {
-  final random = Random();
-  List<double> futurePredictions = [];
-
-  for (int i = 0; i < simulations; i++) {
-    double prediction = historicalData[random.nextInt(historicalData.length)];
-    futurePredictions.add(prediction);
-  }
-
-  return futurePredictions;
-}
-
-double calculateAverage(List<double> data) {
-  return data.reduce((a, b) => a + b) / data.length;
-}
