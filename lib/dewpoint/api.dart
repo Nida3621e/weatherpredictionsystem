14f9a264cd951da0b959356ed6b96cce299fc7ba
Yumna Afsar <83974096+yumnaafsar@users.noreply.github.com>
2024-06-24 11:06:04 -0700

    Update api.dart

--- lib/dewpoint/api.dart
+++ lib/dewpoint/api.dart
@@ -1,16 +1,1 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-Future<Map<String, dynamic>> fetchDewPointData(double latitude, double longitude) async {
-  final response = await http.get(
-    Uri.parse(
-      'https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=dew_point_2m&timezone=GMT',
-    ),
-  );
-
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body);
-  } else {
-    throw Exception('Failed to load dew point data');
-  }
-}
+\
